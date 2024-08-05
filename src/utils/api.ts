620618b83312c9565a3eca247e3abf9dc52e6995
SamRol <1628526495@qq.com>
2024-08-05 13:19:37 +0800

    change API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 //https://samrol-express.me/
-axios.defaults.baseURL = 'http://localhost:3000/';
+//http://localhost:3000/
+axios.defaults.baseURL = 'https://samrol-express.me/';
 
 export async function getMatchByName(name:string){
    return axios.get(`/getmatch/${name}`);
