1f8da5995b99e6ab7156c9d235946fa296519e5c
SamRol <1628526495@qq.com>
2025-05-19 22:49:02 +0800

    Add Google Analytics

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -40,6 +40,22 @@ export default function Home() {
   }
 
   useEffect(()=>{
+
+    //Google Analytics
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-CNLNVT7LF5";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CNLNVT7LF5');
+    `;
+    document.head.appendChild(script2);
+
     // Check Route is /
     if (location.pathname === '/') {
       navigate('/donk');
