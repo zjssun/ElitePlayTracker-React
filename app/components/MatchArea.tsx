17afa27b5cfc865ad3e938218107d96af793c453
SamRol <1628526495@qq.com>
2025-07-15 17:26:43 +0800

    Format the document

--- app/components/MatchArea.tsx
+++ app/components/MatchArea.tsx
@@ -1,30 +1,30 @@
-import {NavLink,Outlet} from "react-router"
-import {playerList,GetPlayerImg} from "../utils/ToolBox"
-import {useTranslation} from 'react-i18next'
+import { NavLink, Outlet } from "react-router"
+import { playerList, GetPlayerImg } from "../utils/ToolBox"
+import { useTranslation } from 'react-i18next'
 
 import "../css/mtachArea.css"
 
-export default function MatchArea(){
-   const {t} = useTranslation();
-   return( 
-      <div className='matchArea-container'>
-          <hgroup className="matchArea-hgroup">
-              {playerList.map(
-                (player)=>(
-                  <NavLink className={({ isActive, isPending }) => 
-                    isPending ? "baseNavLink"
-                    : isActive ? "baseNavLink-active baseNavLink" 
-                    : "baseNavLink"
-                  } 
-                    key={player}
-                    to={`/${player}`}>
-                    {GetPlayerImg(player) ? <img className='avatar-img' src={GetPlayerImg(player)}/> : ""}
-                    <span>{t(`${player}`)}</span>
-                  </NavLink>
-                ))
-              }
-          </hgroup>
-          <Outlet/>
-      </div>
-   )
+export default function MatchArea() {
+  const { t } = useTranslation();
+  return (
+    <div className='matchArea-container'>
+      <hgroup className="matchArea-hgroup">
+        {playerList.map(
+          (player) => (
+            <NavLink className={({ isActive, isPending }) =>
+              isPending ? "baseNavLink"
+                : isActive ? "baseNavLink-active baseNavLink"
+                  : "baseNavLink"
+            }
+              key={player}
+              to={`/${player}`}>
+              {GetPlayerImg(player) ? <img className='avatar-img' src={GetPlayerImg(player)} /> : ""}
+              <span>{t(`${player}`)}</span>
+            </NavLink>
+          ))
+        }
+      </hgroup>
+      <Outlet />
+    </div>
+  )
 }
